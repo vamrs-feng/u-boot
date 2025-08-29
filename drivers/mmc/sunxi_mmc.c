a425ea332b4531f1095b79140d055c27c017f17c
Yixun Lan <dlan@gentoo.org>
2025-08-29 08:12:34 +0000

    sunxi: mmc: a733: Setup clock and reset
    Enable clock and de-assert reset for SDHC controller found in A733 SoC.
    
    Signed-off-by: Yixun Lan <dlan@gentoo.org>

--- drivers/mmc/sunxi_mmc.c
+++ drivers/mmc/sunxi_mmc.c
@@ -564,6 +564,29 @@ static const struct mmc_ops sunxi_mmc_ops = {
 	.init		= sunxi_mmc_core_init,
 };
 
+static void sunxi_mmc_set_clkrst(void *ccm, int sdc_no)
+{
+	/* config ahb clock */
+	debug("init mmc %d clock and io\n", sdc_no);
+#if !defined(CONFIG_SUN50I_GEN_H6) && !defined(CONFIG_SUNXI_GEN_NCAT2)
+	setbits_le32(ccm + CCU_AHB_GATE0, 1 << AHB_GATE_OFFSET_MMC(sdc_no));
+
+#ifdef CONFIG_SUNXI_GEN_SUN6I
+	/* unassert reset */
+	setbits_le32(ccm + CCU_AHB_RESET0_CFG, 1 << AHB_RESET_OFFSET_MMC(sdc_no));
+#endif
+#if defined(CONFIG_MACH_SUN9I)
+	/* sun9i has a mmc-common module, also set the gate and reset there */
+	writel(SUNXI_MMC_COMMON_CLK_GATE | SUNXI_MMC_COMMON_RESET,
+	       SUNXI_MMC_COMMON_BASE + 4 * sdc_no);
+#endif
+#elif  #defined(CONFIG_SUN50I_GEN_H6)
+	setbits_le32(ccm + CCU_H6_MMC_GATE_RESET, (BITR(16) | BIT(0)) << sdc_no);
+#else
+	setbits_le32(ccm + CCU_MMC_GATE_RESET(sdc_no), BIT(16) | BIT(0));
+#endif
+}
+
 struct mmc *sunxi_mmc_init(int sdc_no)
 {
 	void *ccm = (void *)SUNXI_CCM_BASE;
@@ -593,25 +616,8 @@ struct mmc *sunxi_mmc_init(int sdc_no)
 	if (mmc_resource_init(sdc_no) != 0)
 		return NULL;
 
-	/* config ahb clock */
-	debug("init mmc %d clock and io\n", sdc_no);
-#if !defined(CONFIG_SUN50I_GEN_H6) && !defined(CONFIG_SUNXI_GEN_NCAT2)
-	setbits_le32(ccm + CCU_AHB_GATE0, 1 << AHB_GATE_OFFSET_MMC(sdc_no));
+	sunxi_mmc_set_clkrst(ccm, sdc_no);
 
-#ifdef CONFIG_SUNXI_GEN_SUN6I
-	/* unassert reset */
-	setbits_le32(ccm + CCU_AHB_RESET0_CFG, 1 << AHB_RESET_OFFSET_MMC(sdc_no));
-#endif
-#if defined(CONFIG_MACH_SUN9I)
-	/* sun9i has a mmc-common module, also set the gate and reset there */
-	writel(SUNXI_MMC_COMMON_CLK_GATE | SUNXI_MMC_COMMON_RESET,
-	       SUNXI_MMC_COMMON_BASE + 4 * sdc_no);
-#endif
-#else /* CONFIG_SUN50I_GEN_H6 */
-	setbits_le32(ccm + CCU_H6_MMC_GATE_RESET, 1 << sdc_no);
-	/* unassert reset */
-	setbits_le32(ccm + CCU_H6_MMC_GATE_RESET, 1 << (RESET_SHIFT + sdc_no));
-#endif
 	ret = mmc_set_mod_clk(priv, 24000000);
 	if (ret)
 		return NULL;
