238d06aac98fd456322fd257fa51b58465ab7b28
Feng Zhang <feng@radxa.com>
2026-06-10 16:18:33 +0800

    cmd: sunxi_boot_arisc: pass device tree address to SCP via ATF
    Read fdt_addr_r from U-Boot environment and pass it to ATF via
    SMC call (ARM_SVC_ARISC_STARTUP). Validate the device tree magic
    (0xedfe0dd0) before sending and print warning if fdt_addr_r is
    not set, so power management on ARISC can access the full device
    tree for PMIC and regulator configuration.
    
    Signed-off-by: Feng Zhang <feng@radxa.com>

--- cmd/sunxi_boot_arisc.c
+++ cmd/sunxi_boot_arisc.c
@@ -5,6 +5,7 @@
 #include <linux/string.h>
 #include <linux/arm-smccc.h>
 #include <linux/types.h>
+#include <env.h>
 
 static u32 sunxi_smc_call_atf(ulong arg0, ulong arg1, ulong arg2, ulong arg3, ulong pResult)
 {
@@ -19,8 +20,28 @@ static int do_sunxi_boot_arisc(struct cmd_tbl *cmdtp, int flag, int argc,
 		   char *const argv[])
 {
 	u32 ARM_SVC_ARISC_STARTUP = 0x8000ff10;
+	unsigned long dtb_addr;
 
-	sunxi_smc_call_atf(ARM_SVC_ARISC_STARTUP, 0, 0, 0, 0);
+	printf("[SCP] INFO: starting arisc cpu...\n");
+
+	dtb_addr = env_get_hex("fdt_addr_r", 0x1234);
+
+	if (dtb_addr == 0x1234) {
+		printf("[SCP] WARNING: fdt_addr_r is not set, power management may malfunction\n");
+		dtb_addr = 0;
+	} else {
+		printf("[SCP] INFO: fdt_addr_r = 0x%08lx\n", dtb_addr);
+		if (dtb_addr) {
+			u32 magic = *(volatile u32 *)(uintptr_t)dtb_addr;
+			if (magic == 0xedfe0dd0) {
+				printf("[SCP] INFO: dtb magic verified at 0x%08lx\n", dtb_addr);
+			} else {
+				printf("[SCP] WARNING: dtb magic mismatch, expected 0xedfe0dd0 got 0x%08x, power management may malfunction\n", magic);
+			}
+		}
+	}
+
+	sunxi_smc_call_atf(ARM_SVC_ARISC_STARTUP, (ulong)dtb_addr, 0, 0, 0);
 
 	return 0;
 }
